c902486702074a6f3c978e6cd66f215d243e3aa2
piomin <piotr.minkowski@gmail.com>
2020-06-19 23:23:29 +0200

    #1 Fixing errors in compilation

--- order-service/src/main/kotlin/pl/piomin/samples/order/OrderServiceApp.kt
+++ order-service/src/main/kotlin/pl/piomin/samples/order/OrderServiceApp.kt
@@ -1,9 +1,11 @@
 package pl.piomin.samples.order
 
 import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
 import org.springframework.boot.web.client.RestTemplateBuilder
 import org.springframework.context.annotation.Bean
 import org.springframework.web.client.RestTemplate
+import pl.piomin.samples.order.service.EventBus
 
 @SpringBootApplication
 class OrderServiceApp {
@@ -11,4 +13,11 @@ class OrderServiceApp {
     @Bean
     fun restTemplate(): RestTemplate = RestTemplateBuilder().build()
 
+    @Bean
+    fun eventBus(): EventBus = EventBus()
+
+}
+
+fun main(args: Array<String>) {
+    runApplication<OrderServiceApp>(*args)
 }
--- product-service/src/main/kotlin/pl/piomin/samples/product/ProductServiceApp.kt
+++ product-service/src/main/kotlin/pl/piomin/samples/product/ProductServiceApp.kt
@@ -1,7 +1,23 @@
 package pl.piomin.samples.product
 
 import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+import org.springframework.boot.web.client.RestTemplateBuilder
+import org.springframework.context.annotation.Bean
+import org.springframework.web.client.RestTemplate
+import pl.piomin.samples.product.service.EventBus
 
 @SpringBootApplication
 class ProductServiceApp {
+
+    @Bean
+    fun restTemplate(): RestTemplate = RestTemplateBuilder().build()
+
+    @Bean
+    fun eventBus(): EventBus = EventBus()
+
+}
+
+fun main(args: Array<String>) {
+    runApplication<ProductServiceApp>(*args)
 }
