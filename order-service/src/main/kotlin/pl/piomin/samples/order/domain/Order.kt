d662c889fe837797ed479941ac0412fef4da3d23
piomin <piotr.minkowski@gmail.com>
2020-06-22 13:00:20 +0200

    #5 Fix table name

--- order-service/src/main/kotlin/pl/piomin/samples/order/domain/Order.kt
+++ order-service/src/main/kotlin/pl/piomin/samples/order/domain/Order.kt
@@ -1,11 +1,9 @@
 package pl.piomin.samples.order.domain
 
-import javax.persistence.Entity
-import javax.persistence.GeneratedValue
-import javax.persistence.GenerationType
-import javax.persistence.Id
+import javax.persistence.*
 
 @Entity
+@Table(name = "orders")
 data class Order(@Id @GeneratedValue(strategy = GenerationType.AUTO) val id: Int? = null,
                  val productId: Int = 0,
                  val count: Int = 0,
