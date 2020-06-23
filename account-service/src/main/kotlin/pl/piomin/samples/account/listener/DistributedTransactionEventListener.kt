0e037c0493b7beb13ed999596b1ac97e83763a88
piomin <piotr.minkowski@gmail.com>
2020-06-23 23:17:38 +0200

    #7 Fixing error in creating queue on RabbitMQ + adding Actuator

--- account-service/src/main/kotlin/pl/piomin/samples/account/listener/DistributedTransactionEventListener.kt
+++ account-service/src/main/kotlin/pl/piomin/samples/account/listener/DistributedTransactionEventListener.kt
@@ -1,5 +1,9 @@
 package pl.piomin.samples.account.listener
 
+import org.springframework.amqp.core.ExchangeTypes
+import org.springframework.amqp.rabbit.annotation.Exchange
+import org.springframework.amqp.rabbit.annotation.Queue
+import org.springframework.amqp.rabbit.annotation.QueueBinding
 import org.springframework.amqp.rabbit.annotation.RabbitListener
 import org.springframework.stereotype.Component
 import pl.piomin.samples.account.client.DistributedTransaction
@@ -8,7 +12,10 @@ import pl.piomin.samples.account.service.EventBus
 @Component
 class DistributedTransactionEventListener(val eventBus: EventBus) {
 
-    @RabbitListener(queues = ["trx-events"])
+    @RabbitListener(bindings = [
+        QueueBinding(exchange = Exchange(type = ExchangeTypes.TOPIC, name = "trx-events"),
+                value = Queue("trx-events-account"))
+    ])
     fun onMessage(transaction: DistributedTransaction) {
         eventBus.sendTransaction(transaction)
     }
--- product-service/src/main/kotlin/pl/piomin/samples/product/listener/DistributedTransactionEventListener.kt
+++ product-service/src/main/kotlin/pl/piomin/samples/product/listener/DistributedTransactionEventListener.kt
@@ -1,5 +1,9 @@
 package pl.piomin.samples.product.listener
 
+import org.springframework.amqp.core.ExchangeTypes
+import org.springframework.amqp.rabbit.annotation.Exchange
+import org.springframework.amqp.rabbit.annotation.Queue
+import org.springframework.amqp.rabbit.annotation.QueueBinding
 import org.springframework.amqp.rabbit.annotation.RabbitListener
 import org.springframework.stereotype.Component
 import pl.piomin.samples.product.client.DistributedTransaction
@@ -8,7 +12,10 @@ import pl.piomin.samples.product.service.EventBus
 @Component
 class DistributedTransactionEventListener(val eventBus: EventBus) {
 
-    @RabbitListener(queues = ["trx-events"])
+    @RabbitListener(bindings = [
+        QueueBinding(exchange = Exchange(type = ExchangeTypes.TOPIC, name = "trx-events"),
+                value = Queue("trx-events-product"))
+    ])
     fun onMessage(transaction: DistributedTransaction) {
         eventBus.sendTransaction(transaction)
     }
